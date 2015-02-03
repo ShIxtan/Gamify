dccafc3bd6dd2c6ec500b994794fd6fc77abccc3
ShIxtan <aulbrich@iu.edu>
2015-02-03 13:58:39 -0800

    allow deleting habits

--- app/assets/javascripts/views/habits/indexItem.js
+++ app/assets/javascripts/views/habits/indexItem.js
@@ -2,8 +2,18 @@ GamifyApp.Views.HabitIndexItem = Backbone.CompositeView.extend({
   template: JST['habits/indexItem'],
   tagname: "li class='habit task'",
 
+  events: {
+    "click span": "deleteTask"
+  },
+
   render: function(){
     this.$el.html(this.template({habit: this.model}))
     return this;
+  },
+
+  deleteTask: function(event){
+    event.preventDefault()
+    this.model.destroy()
+    this.remove()
   }
 })
