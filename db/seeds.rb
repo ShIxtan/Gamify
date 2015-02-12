f3be317e1ccd947df4f877e915dafc5391e8af07
ShIxtan <aulbrich@iu.edu>
2015-02-12 12:42:28 -0800

    seed was looking for id of 0

--- db/seeds.rb
+++ db/seeds.rb
@@ -150,7 +150,7 @@ end
 [Daily, Reward, Habit, Todo].each do |class_type|
   class_type.all.each do |task|
     ids = []
-    rand*4.times { ids.push(rand*4) }
+    rand*5.times { ids.push(rand*4 + 1) }
     task.tag_ids = ids
   end
 end
